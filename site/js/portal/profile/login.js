5c5c68567587a9eda75fdadfc972bcdeed856752
= <=>
2011-06-16 09:53:39 -0700

    Updating to current

--- site/js/portal/profile/login.js
+++ site/js/portal/profile/login.js
@@ -1,3 +1,4 @@
+/*global $, JQuery, debug, portal*/
 /**
  * Login User.  Validate, handle response.
  *
@@ -9,12 +10,12 @@ function Login(options) {
 			moddate: "2010-7-31 19:52",
 			createdate: "2010-7-31 19:52",
 			cookie: 'HiveLMS',
-			user: 'student@hivelms.com',
+			user: 'student@hivelms.com'
 		},
+		cookieEmail = $.cookie('email'),
 		settings = $.extend(defaults, options);
 	//$("#password").chromaHash({bars: 3, salt:"7be82b35cb0199120eea35a4507c9acf", minimum:6});
 	// Populate Username from Cookie if it exists
-	var cookieEmail = $.cookie('email');
 	if(cookieEmail !== null && cookieEmail !== undefined) {
 		$('#accountName').val(cookieEmail);
 	} else {
@@ -22,18 +23,62 @@ function Login(options) {
 	}
 	
 	///////////////// Private Functions ////////////////////////////////
-	function handleResponse(_obj) {
-		switch(_obj.status) {
+	function enableInput(type, target) {
+		if(type) {
+			target.removeAttr('disabled')
+				  .removeClass('processing'); 
+			target.find('span span').html('Log In');
+			$('.active').removeClass('active'); // Clean up touch selections
+		} else {
+			target.find('span span').html('Processing...');
+			target.removeClass('hover')
+                   .addClass('processing')
+                   .attr('disabled', 'disabled');
+		}
+	}
+	
+	// Maybe offload these to a utility object
+	function getScreenSize() {
+		if (window.screen) {
+			return window.screen.width+"x"+window.screen.height;
+		} else {
+			return 'Unknown';
+		}
+	}
+	
+	function getPixelDepth() {
+		if (window.screen.pixelDepth) {
+			return window.screen.pixelDepth;
+		} else if (window.screen.colorDepth) {
+   			return window.screen.colorDepth;
+		} else {
+			return 'Unknown';
+		}
+	}
+	// End Utility
+	
+	function setStatusMsg(msg, icon) {
+		var nmsg = msg ? msg : "";
+		$('#login_status').removeClass('success error fail').addClass(icon).html(nmsg);
+	}
+	
+	function isValidEmailAddress(email) {
+		var pattern = new RegExp(/^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+\.[a-zA\-Z]{2,4}$/);
+		return pattern.test(email);
+	}
+	
+	function handleResponse(obj) {
+		switch(obj.status) {
 			case 'success':
 				portal.playAudio('success');
 				// We'll get back the profile object, set it locally to the Login object
-				settings.profile = _obj.login.profile;
+				settings.profile = obj.login.profile;
 				
 				// Lets set up some Cookie stuff 
 				$.cookie('email', settings.profile.email, {expires: 30}); // set username in cookie
 				$('#accountName').val(settings.profile.email);
 				// Build Assignments - Note: I wanted to elminate another round trip so it comes back with the profile
-				portal.assignments.init(_obj.assignment);
+				portal.assignments.init(obj.assignment);
 				portal.navigation.goTo('#assignments', 'slide'); // Note flip, cube and swap a little visually buggy in chrome.
 				enableInput(true, $('#login_btn'));
 				// remove success icon
@@ -54,22 +99,12 @@ function Login(options) {
 				debug('Login: Sorry, unexpected type received via Login', 2);
 			break;
 		}
-		setStatusMsg(_obj.login.msg, _obj.status); // msg, icon
-	}
-	
-	function setStatusMsg(_msg, _icon) {
-		var msg = _msg ? _msg : "";
-		$('#login_status').removeClass('success error fail').addClass(_icon).html(msg);
-	}
-	
-	function isValidEmailAddress(_email) {
-		var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-		return pattern.test(_email);
+		setStatusMsg(obj.login.msg, obj.status); // msg, icon
 	}
 	
 	function validateUser(user, pass, target) {
 		enableInput(false, target);
-		var $this = this;
+		var $this = this, data_obj;
 		if(!isValidEmailAddress(user)) {
 			$('#accountName').addClass('inputerror');
 			// Come back and whip up some validation later
@@ -82,7 +117,7 @@ function Login(options) {
 			$('#password').removeClass('inputerror');
 			debug("Validating " + user, 4);
 			
-			var data_obj = {
+			data_obj = {
 	            action: "Login",
 	            email: user,
 	            password: pass,
@@ -92,39 +127,6 @@ function Login(options) {
 			portal.communication.sendMessage(data_obj, handleResponse, "login.js: validateUser");
 		}
 	}
-	
-	function enableInput(type, target) {
-		if(type) {
-			target.removeAttr('disabled')
-				  .removeClass('processing'); 
-			target.find('span span').html('Log In');
-			$('.active').removeClass('active'); // Clean up touch selections
-		} else {
-			target.find('span span').html('Processing...');
-			target.removeClass('hover')
-                   .addClass('processing')
-                   .attr('disabled', 'disabled');
-		}
-	}
-	// Maybe offload these to a utility object
-	function getScreenSize() {
-		if (window.screen) {
-			return window.screen.width+"x"+window.screen.height;
-		} else {
-			return 'Unknown';
-		}
-	}
-	
-	function getPixelDepth() {
-		if (window.screen.pixelDepth) {
-			return window.screen.pixelDepth;
-		} else if (window.screen.colorDepth) {
-   			return window.screen.colorDepth;
-		} else {
-			return 'Unknown';
-		}
-	}
-	// End Utility
 	
 	///////////////// Public Functions ////////////////////////////////
 	this.init = function() {
